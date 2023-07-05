876ff5fb686244d9bc5ec3022a4551e6bd1d072a
bobo <bobo@pesto.io>
2023-07-05 09:45:44 +0200

    editor view

--- frontend/oci/example_expo_app3/components/pestoEditor.tsx
+++ frontend/oci/example_expo_app3/components/pestoEditor.tsx
@@ -30,28 +30,70 @@ export default function PestoEditor(props: any) {
         }}
         keyboardShouldPersistTaps="handled">
         <View style={styles.container}>
+        <View style={styles.userWrapper}>
         <Text style={styles.addText}> en cour ...</Text>
         <Pressable onPress={() => handleClick()}>
             <Text style={styles.addText}> back </Text>
         </Pressable>
+        </View>
         </View>
       </ScrollView>
     )
 }
 
 const styles = StyleSheet.create({
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "flex-end",
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
   container: {
     flex: 1,
     backgroundColor: "#E5E5E5",
   },
+  userWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: "bold",
+  },
   items: {
     marginTop: 30,
   },
+  writeUserWrapper: {
+    position: "absolute",
+    bottom: 60,
+    width: "100%",
+    marginLeft: 10,
+    flexDirection: "row",
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
   input: {
-    paddingVertical: 15,
+    paddingVertical: 7,
     paddingHorizontal: 15,
     backgroundColor: "#FFF",
-    width: 250,
+    width: 200,
+  },
+  addUser: {
+    width: 60,
+    height: 60,
+    marginHorizontal: 20,
+    backgroundColor: "#FFF",
+    justifyContent: "center",
+    alignItems: "center",
   },
   addText: {},
 });
